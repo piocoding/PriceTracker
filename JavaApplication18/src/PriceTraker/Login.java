d32f37532538ec9fcf93556e24363474e3120a02
piocoding <151313504+piocoding@users.noreply.github.com>
2023-12-29 10:15:50 +0800

    Update Login.java

--- JavaApplication18/src/PriceTraker/Login.java
+++ JavaApplication18/src/PriceTraker/Login.java
@@ -6,27 +6,29 @@ import java.sql.*;
 //make sure accounts table is already created 
 // 2 columns, (username, PRIMARY KEY, varchar(20)), (password, varchar(20))
 public class Login {
+    private boolean status;
     
     public Login(String u){
         Scanner scn = new Scanner(System.in);
         
-        boolean status = false;
-        while ( status == false ) {
+        status = false;
+        while ( !status ) {
             System.out.println("1. Login \n2. Registration");
             int ans = scn.nextInt();
             if ( ans == 1 ) {
-                LoginUI(status, u);
+                LoginUI(u);
             }
             else if ( ans == 2 ) {
-                RegistrationUI(status, u);
+                RegistrationUI(u);
             }
             else {
                 System.out.println("invalid choice.");
             }
         }
+        
     }
     
-    public void LoginUI(boolean status, String u){
+    public void LoginUI(String u){
         Scanner scn = new Scanner(System.in);
         
         System.out.println("–Login–––");
@@ -36,15 +38,15 @@ public class Login {
         System.out.print("Password: ");
         String pass = scn.next();
         
-        String url = "jdbc:mysql://localhost:3306/accounts";
-        String username = "root";
-        String password = "Allahswt24434!";
+        String url = "jdbc:derby://localhost:1527/pricetrackerDB";
+        String username = "javasofia";
+        String password = "javasofia";
         
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
             Connection connection = DriverManager.getConnection(url,username,password);
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM accounts WHERE username = '" + user + "' and password = '" + pass + "';");
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM APP.ACCOUNTS WHERE username = '" + user + "' and password = '" + pass + "'");
             
             if (resultSet.next() ){
                 System.out.println("Successfully logged in.");
@@ -62,7 +64,7 @@ public class Login {
         }
     }
     
-    public void RegistrationUI(boolean status){
+    public void RegistrationUI(String u){
         Scanner scn = new Scanner(System.in);
         
         System.out.println("–Registeration–––");
@@ -71,22 +73,21 @@ public class Login {
         System.out.print("Set Password: ");
         String pass = scn.next();
         
-        String url = "jdbc:mysql://localhost:3306/accounts";
-        String username = "root";
-        String password = "Allahswt24434!";
+        String url = "jdbc:derby://localhost:1527/pricetrackerDB";
+        String username = "javasofia";
+        String password = "javasofia";
         
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
             Connection connection = DriverManager.getConnection(url,username,password);
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("INSERT INTO APP.TRYACCOUNTS VALUES ('" + user + "', '" + pass + "')\n SELECT * FROM accounts;");
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO APP.ACCOUNTS VALUES ('" + user + "', '" + pass + "')");
+            statement.executeUpdate();
             connection.close();
             System.out.println("Successfully registered account.");
-            LoginUI(status, u);
+            LoginUI(u);
         }
         catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println("ERROR creating account. (There might already exist an account with the same username.)");
+            System.out.println("There exists another user with the same username.");
         }
     }
 }
