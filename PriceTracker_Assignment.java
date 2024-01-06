0c63b43b196bb1bd74de43806167e512ab9ecdd2
izzanishayuri <154729900+izzanishayuri@users.noreply.github.com>
2024-01-06 09:04:55 +0800

    Add files via upload

---
+++ PriceTracker_Assignment.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pricetracker_assignment;
+
+/**
+ *
+ * @author user
+ */
+public class PriceTracker_Assignment {
+    
+   public static void main(String[] args) {
+
+        FuzzySearch search = new FuzzySearch();
+        search.userInput();
+        
+    }
+    
+}
